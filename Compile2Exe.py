d47909543abe9de28fab3b6959f3380b1350ef9b
James Haywood <jhaywood@parktudor.org>
2018-03-02 11:43:10 -0500

    3/2/18 pages

--- Compile2Exe.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-build_exe_options = {'packages': ['dbm']}
-setup(name='The Seven Suns',
-      version='1.1',
-      options = {"build_exe": build_exe_options},
-      description='The Seven Suns',
-      executables = [Executable("SevenSuns.py")])
